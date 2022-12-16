c79f9017e729dcda63a634c15c018f3b950d43c3
freestyle2018 <troinfo@yandex.ru>
2022-12-16 17:19:25 +0300

    Сортировка и фильтрация

--- blog/app/Models/Post.php
+++ blog/app/Models/Post.php
@@ -6,11 +6,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Orchid\Attachment\Attachable;
+use Orchid\Filters\Filterable;
 use Orchid\Screen\AsSource;
 
 class Post extends Model
 {
-    use AsSource, Attachable;
+    use AsSource, Attachable, Filterable;
 
     /**
      * @var array
@@ -21,5 +22,25 @@ class Post extends Model
         'body',
         'author',
         'hero'
+    ];
+
+    /**
+     * Name of columns to which http sorting can be applied
+     *
+     * @var array
+     */
+    protected $allowedSorts = [
+        'title',
+        'created_at',
+        'updated_at'
+    ];
+
+    /**
+     * Name of columns to which http filter can be applied
+     *
+     * @var array
+     */
+    protected $allowedFilters = [
+        'title',
     ];
 }
